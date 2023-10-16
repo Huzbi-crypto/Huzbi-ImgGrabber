e44a5ca57339d1a651025815af845c9c0d3bb435
Huzbi-crypto <huzbinaru@gmail.com>
2023-10-16 21:51:32 +0500

    Changes to be committed: 	new file:   icons/128px.png 	new file:   icons/16px.png 	new file:   icons/32px.png 	new file:   icons/64px.png 	new file:   manifest.json 	new file:   popup.css 	new file:   popup.html 	new file:   popup.js
    👾 Added source files and assets for the extension.

---
+++ popup.js
@@ -0,0 +1,47 @@
+const grabBtn = document.getElementById('grabBtn');
+grabBtn.addEventListener('click', () => {
+    chrome.tabs.query({active: true}, (tabs) => {
+        const tab = tabs[0];
+        if (tab) {
+            // alert(tab.id)
+            // chrome.scripting.executeScript(
+            //     {
+            //         target: {tabId: tab.id, allFrames: true},
+            //         function: grabImages
+            //     },
+            //     onResult
+            // )
+            execScript(tab)
+        } else {
+            alert('No active tab found')
+        }
+    })
+})
+
+function execScript(tab) {
+    chrome.scripting.executeScript(
+        {
+            target: {tabId: tab.id, allFrames: true},
+            function: grabImages
+        },
+        onResult
+    )
+}
+
+function grabImages() {
+    const images = document.querySelectorAll('img');
+    return Array.from(images).map(image=>image.src);
+}
+
+function onResult(frames) {
+    if (!frames || !frames.length) {
+        alert("Could not retrieve images from specified page");
+        return;
+    }
+
+    const imageUrls = frames.map(frame=>frame.result).reduce((r1, r2) => r1.concat(r2));
+
+    window.navigator.clipboard.writeText(imageUrls.join("\n")).then(() => {
+        window.close();
+    })
+}
